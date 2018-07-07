63e679d1725c00289ffb1f38248c52750d6416e5
ElahiKeru <carnatorian@gmail.com>
2018-07-06 20:19:09 -0500

    Implemeted Object
    Added object to implementation of wage randomizer to hold monetary denominations

--- WageRandomizer/Program.cs
+++ WageRandomizer/Program.cs
@@ -3,68 +3,83 @@ using System.IO;
 
 namespace WageRandomizer
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
             Random r = new Random();
-            StreamReader sr = new StreamReader(@"Roster.txt");
             DateTime timeStamp = DateTime.Now;
-            StreamWriter sw = new StreamWriter(timeStamp.ToString("yyyyMMdd-HHmmss.fff") + @"_Receipt.txt");
             int platTotal = 0;
             int goldTotal = 0;
             int silverTotal = 0;
             int copperTotal = 0;
 
-            string line = sr.ReadLine();
-
-            while (line != null)
+            using (StreamWriter sw = new StreamWriter(timeStamp.ToString("yyyyMMdd-HHmmss.fff") + @"_Receipt.txt"))
             {
-                string[] data = line.Split(',');
-
-                int platBit = 0;
-                int goldBit = 0;
-                int silverBit = 0;
-                int copperBit = 0;
-
-                if (int.Parse(data[1]) > 0)
+                using (StreamReader sr = new StreamReader(@"Roster.txt"))
                 {
-                    platBit = r.Next(1, int.Parse(data[1])+1);
+                    string line = sr.ReadLine();
+
+                    while (line != null)
+                    {
+                        //data[0] Name
+                        //data[1] Platinum
+                        //data[2] Gold
+                        //data[3] Silver
+                        //data[4] Copper
+                        //data[5] Active
+                        string[] data = line.Split(',');
+
+                        DataCollection dc = new DataCollection();
+                        dc.ParseData(data);
+
+                        if (dc.Active)
+                        {
+                            int platBit = 0;
+                            int goldBit = 0;
+                            int silverBit = 0;
+                            int copperBit = 0;
+
+                            if (dc.Platinum > 0)
+                            {
+                                platBit = r.Next(1, dc.Platinum + 1);
+                            }
+
+                            if (dc.Gold > 0)
+                            {
+                                goldBit = r.Next(1, dc.Gold + 1);
+                            }
+
+                            if (dc.Silver > 0)
+                            {
+                                silverBit = r.Next(1, dc.Silver + 1);
+                            }
+
+                            if (dc.Copper > 0)
+                            {
+                                copperBit = r.Next(1, dc.Copper + 1);
+                            }
+
+                            sw.WriteLine($"{dc.Name} has expended {platBit} Platinum, {goldBit} Gold, {silverBit} Silver, and {copperBit} Copper.");
+                            Console.WriteLine($"{dc.Name} has expended {platBit} Platinum, {goldBit} Gold, {silverBit} Silver, and {copperBit} Copper.");
+
+                            platTotal += platBit;
+                            goldTotal += goldBit;
+                            silverTotal += silverBit;
+                            copperTotal += copperBit;
+
+                            sw.Flush();
+                        }
+                        line = sr.ReadLine();
+                    }
                 }
 
-                if (int.Parse(data[2]) > 0)
-                {
-                    goldBit = r.Next(1, int.Parse(data[2])+1);
-                }
-
-                if (int.Parse(data[3]) > 0)
-                {
-                    silverBit = r.Next(1, int.Parse(data[3])+1);
-                }
-
-                if (int.Parse(data[4]) > 0)
-                {
-                    copperBit = r.Next(1, int.Parse(data[4])+1);
-                }
-
-                sw.WriteLine($"{data[0]} has expended {platBit} Platinum, {goldBit} Gold, {silverBit} Silver, and {copperBit} Copper.");
-                Console.WriteLine($"{data[0]} has expended {platBit} Platinum, {goldBit} Gold, {silverBit} Silver, and {copperBit} Copper.");
-
-                platTotal += platBit;
-                goldTotal += goldBit;
-                silverTotal += silverBit;
-                copperTotal += copperBit;
-
+                Console.WriteLine();
+                sw.WriteLine();
+                sw.WriteLine($"Totals:\r\nPlatinum: {platTotal}\r\nGold: {goldTotal}\r\nSilver: {silverTotal}\r\nCopper: {copperTotal}");
+                Console.WriteLine($"Totals:\r\nPlatinum: {platTotal}\r\nGold: {goldTotal}\r\nSilver: {silverTotal}\r\nCopper: {copperTotal}");
                 sw.Flush();
-                line = sr.ReadLine();
             }
-
-            Console.WriteLine();
-            sw.WriteLine();
-            sw.WriteLine($"Totals:\r\nPlatinum: {platTotal}\r\nGold: {goldTotal}\r\nSilver: {silverTotal}\r\nCopper: {copperTotal}");
-            Console.WriteLine($"Totals:\r\nPlatinum: {platTotal}\r\nGold: {goldTotal}\r\nSilver: {silverTotal}\r\nCopper: {copperTotal}");
-            sw.Flush();
-
             Console.ReadLine();
         }
     }
